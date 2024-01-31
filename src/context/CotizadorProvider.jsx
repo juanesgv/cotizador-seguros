dc19c704255aeacde8e712f8619261760c1a5ce0
juanesgv <juanesgarviedma23@gmail.com>
2024-01-31 13:33:40 -0500

    algoritmo para definir el tipo de cotización del seguro, dependiendo de la marca, año y plan

--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { getYearDifference, calcularMarca, calcularPlan, formatearDinero } from "../helpers";
 
 const CotizadorContext = createContext()
 
@@ -11,14 +12,35 @@ const CotizadorProvider = ({children}) =>{
     })
 
     const [error, setError] =useState('')
+    const [resultado, setResultado] =useState(0)
 
     const handleChangeDatos = e => {
         setDatos({
             ...datos,
             [e.target.name] : e.target.value,
-            error,
-            setError
         })
+
+    }
+
+    const cotizarSeguro = () => {
+        //base
+        let resultado = 100000;
+
+        //Obtener diferencia de años
+        const  diferencia = getYearDifference(datos.year)
+
+        //Restar el 3% por cada año
+        resultado -= ((diferencia * 3) * resultado)/ 100
+
+        //Americano 15%, Europeo 30%, Asiatico 5%
+        resultado *= calcularMarca(datos.marca)
+
+        //Básico 20% completo 50%
+        resultado *= calcularPlan(datos.plan)
+
+        //formatear dinero
+        resultado = formatearDinero(resultado)
+        setResultado(resultado)
     }
     
     return(
@@ -27,7 +49,8 @@ const CotizadorProvider = ({children}) =>{
                 datos,
                 handleChangeDatos,
                 error,
-                setError
+                setError,
+                cotizarSeguro
             }}
         >
             {children}
